30914dd057fad4ec186bc0afdb6754a6367563ee
Jacek Koziński <jacek.koz@gmail.com>
2013-12-20 21:04:42 +0100

    Draft

--- client.c
+++ client.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
+#include <pthread.h>
 
-int main()
+int main(int arguments_number, char* arguments[])
 {
+    int resourceType = atoi(arguments[1]);
+    int resourceAmount = atoi(arguments[2]);
+    int workTime = atoi(arguments[3]);
+    int partnerPID;
+
+    while (1)
+    {
+        // Communicate the need for resource
+
+        // Sleep on condition until woken up to work
+
+        // Receive co-worker PID, server has locked resources
+        // partnerPID = recieve();
+
+        // Communicate start of work
+        printf("%d %d %d %d\n", resourceType, resourceAmount, getpid(), partnerPID);
+
+        // Actual work
+        sleep(workTime);
+
+        // Coummunicate the release of resources
+
+        // Communicate end of work
+        printf("%d KONIEC\n", getpid());
+    }
+
     return 0;
 }
--- server.c
+++ server.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
+#include <pthread.h>
 
-int main()
+int main(int arguments_number, char* arguments[])
 {
+    // Initializing input data
+    int resourcesNumber = atoi(arguments[1]);
+    int resourcesAmount = atoi(arguments[2]);
+    int resources[resourcesNumber], loopCounter;
+
+    // Initializing the array of resources
+    for (loopCounter = 0; loopCounter < resourcesNumber; loopCounter++)
+    {
+        resources[loopCounter] = resourcesAmount;
+    }
+
+    while (1)
+    {
+        // Main body
+    }
+
     return 0;
 }
